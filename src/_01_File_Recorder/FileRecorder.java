2914690b27992552c809e758d4994c75d633a205
ryanoh0303 <ryano35961@gmail.com>
2020-04-05 11:49:23 -0700

    day1

--- src/_01_File_Recorder/FileRecorder.java
+++ src/_01_File_Recorder/FileRecorder.java
@@ -1,5 +1,17 @@
 package _01_File_Recorder;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 public class FileRecorder {
 	// Create a program that takes a message from the user and saves it to a file.
+	public static void main(String[] args) throws IOException {
+		String input = JOptionPane.showInputDialog("TYPE A Message");
+		FileWriter fw = new FileWriter("src/_01_File_Recorder/test3.txt");
+		fw.write(input);
+		fw.close();
+		
+	}
 }
--- src/_02_File_Encrypt_Decrypt/FileDecryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileDecryptor.java
@@ -1,13 +1,44 @@
 package _02_File_Encrypt_Decrypt;
 
-public class FileDecryptor {
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
+public class FileDecryptor{
+	public static void main(String[] args) throws IOException{
+		FileReader fr = new FileReader("src/_02_File_Encrypt_Decrypt/text.txt");
+		String finaltoPrint = "";
+		int word =0;
+		while(word!= -1){
+			System.out.print(word);
+			word = fr.read();
+			if((char) word == ' ' ) {
+				finaltoPrint= finaltoPrint +" ";
+			}
+			
+			else if(word<=100) {
+	
+			   finaltoPrint = finaltoPrint+  (char)(word+22);
+			}
+			else if(word>100) {
+				finaltoPrint += (char)(word-4);
+			}
+			
+		}
+		fr.close();
+		//System.out.println(finaltoPrint);
+		JOptionPane.showMessageDialog(null, finaltoPrint);
+	}
+	
 	/*
 	 * Decryption is the process of taking encoded or encrypted text or other data
-	 * and converting it back into text that you or the computer can read and understand.
 	 *
 	 * The shift cipher is decrypted by using using the key and shifting back up,
 	 * at the end of our encryption example we had our alphabet as:
-	 *
+	 *	 * and converting it back into text that you or the computer can read and understand.
+
 	 * e f g h i j k l m n o p q r s t u v w x y z a b c d
 	 *
 	 * If we shift it back up by 4 based on the key we used the alphabet is decrypted:
