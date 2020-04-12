c5fa4c09e6820ddfccf41be641bedeee83f969e3
ryanoh0303 <ryano35961@gmail.com>
2020-04-12 11:36:59 -0700

    work on next week

--- src/_02_File_Encrypt_Decrypt/FileEncryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileEncryptor.java
@@ -1,5 +1,7 @@
 package _02_File_Encrypt_Decrypt;
 
+import javax.swing.JOptionPane;
+
 public class FileEncryptor {
 	/*
 	 * Encryption is the process of encoding a message or information
@@ -20,4 +22,37 @@ public class FileEncryptor {
 	 * Create a program that takes a message and a key from the user.
 	 * Use the key to shift each letter in the users input and save the final result to a file.
 	 */
+	public static void main(String[] args) {  //122-> 100
+		                                      //121 ->99
+		String finalWord = "";
+		int key = Integer.parseInt(JOptionPane.showInputDialog("KEY: "));
+		String word = JOptionPane.showInputDialog("WORD: ");
+		for(int i=0; i<word.length(); i++) {
+			if(word.charAt(i)==' ') {
+				finalWord += ' ';
+			}
+			if(Character.isUpperCase(word.charAt(i))) {
+				String x= (""+word.charAt(i)).toLowerCase();
+				char xy = x.charAt(0);
+				if((int) xy >= 122-key) {
+					 finalWord += (char)(-1* (int) xy + 222) ;
+				}
+				else {
+					finalWord += (char) (key+ (int)xy);
+				}	
+			}
+			else {			
+			if((int) word.charAt(i) >= 122-key) {
+				 finalWord += (char)(-1* (int) word.charAt(i) + 222) ;
+			}
+			else {
+				finalWord += (char) (key+ (int)word.charAt(i));
+			}
+		}
+		}
+		System.out.println("KEY: "+ key);
+		System.out.println("WORD: "+ word);
+		System.out.println("FINAL WORD: "+ finalWord);
+		
+	}
 }
--- src/_03_To_Do_List/ToDoList.java
+++ src/_03_To_Do_List/ToDoList.java
@@ -1,8 +1,29 @@
 package _03_To_Do_List;
 
-public class ToDoList {
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class ToDoList implements MouseListener, ActionListener {
+	static  JFrame window;
+	static JButton addTask;
+	static JButton viewTask;
+	static JButton removeTask;
+	static JButton saveList;
+	static JButton loadList;
+	static JPanel jpanel;
+	static ArrayList<String> tasks;
 	/*
 	 * Create a program with five buttons, add task, view tasks, remove task, save list, and load list. 
+	
 	 *
 	 * When add task is clicked:
 	 * 		Create a JOptionPane to ask the user for a task and add it to an ArrayList
@@ -21,4 +42,74 @@ public class ToDoList {
 	 * 
 	 * When the program starts, it should automatically load the last saved file into the list. 
 	 */
+	public static void main(String[] args) {
+		window =  new JFrame("To do List");
+		jpanel = new JPanel();
+		 addTask = new JButton("Add Task");
+		 viewTask = new JButton("View Task");
+		 removeTask = new JButton("Remove Task");
+		 saveList = new JButton("Save List");
+         loadList = new JButton("Load Task");          
+         
+         ToDoList td = new ToDoList();
+         td.run();
+		
+	}
+	public void run() {
+		window.setSize(800, 800);
+		addTask.setSize(new Dimension(200,200));
+		viewTask.setSize(new Dimension(200,200));
+		removeTask.setSize(new Dimension(200,200));
+		saveList.setSize(new Dimension(200,200));
+		loadList.setSize(new Dimension(200,200));
+		jpanel.add(addTask);
+		jpanel.add(viewTask);
+		jpanel.add(removeTask);
+		jpanel.add(saveList);
+		jpanel.add(loadList);
+		addTask.addActionListener(this);
+		viewTask.addActionListener(this);
+		removeTask.addActionListener(this);
+		saveList.addActionListener(this);
+		loadList.addActionListener(this);
+		window.add(jpanel);
+		window.setVisible(true);
+	}
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+	
+		
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub 					//code over here
+		if(e.getSource() == addTask) {
+			String tasktoAdd = JOptionPane.showInputDialog("What task to add?");
+			tasks.add(tasktoAdd);
+		}
+		if(e.getSource() == viewTask) {
+			
+		}
+	}
 }
