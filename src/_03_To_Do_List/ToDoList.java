da7fee9bfc78435e5bc9fb445e455415a013edfd
ryanoh0303 <ryano35961@gmail.com>
2020-04-12 12:58:20 -0700

    work on it next time

--- src/_03_To_Do_List/ToDoList.java
+++ src/_03_To_Do_List/ToDoList.java
@@ -5,6 +5,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
@@ -20,7 +24,7 @@ public class ToDoList implements MouseListener, ActionListener {
 	static JButton saveList;
 	static JButton loadList;
 	static JPanel jpanel;
-	static ArrayList<String> tasks;
+	 ArrayList<String> tasks =  new ArrayList<>();
 	/*
 	 * Create a program with five buttons, add task, view tasks, remove task, save list, and load list. 
 	
@@ -42,7 +46,7 @@ public class ToDoList implements MouseListener, ActionListener {
 	 * 
 	 * When the program starts, it should automatically load the last saved file into the list. 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		window =  new JFrame("To do List");
 		jpanel = new JPanel();
 		 addTask = new JButton("Add Task");
@@ -109,7 +113,22 @@ public class ToDoList implements MouseListener, ActionListener {
 			tasks.add(tasktoAdd);
 		}
 		if(e.getSource() == viewTask) {
-			
+			System.out.println(tasks);
+		}
+		if(e.getSource() ==removeTask) {
+			String tasktoRemove = JOptionPane.showInputDialog("What task to remove?");
+			tasks.remove(tasks.indexOf(tasktoRemove));
+		}
+		if(e.getSource() == saveList) {
+			try {
+				PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("tasks.txt")));
+				pw.write(""+tasks);
+				pw.close();
+				
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 		}
 	}
 }
